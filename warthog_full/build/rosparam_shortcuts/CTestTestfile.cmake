4f3cc2e2ec2df0c13d71e73766b090b528db58e3
cuong <bonx771@gmail.com>
2026-02-24 09:54:30 +0700

    Remove build and devel from tracking

--- warthog_full/build/rosparam_shortcuts/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cuong/warthog_full/src/rosparam_shortcuts
-# Build directory: /home/cuong/warthog_full/build/rosparam_shortcuts
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
